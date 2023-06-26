402529fcdb491555cc9dd7093b4ae86e46c04cf3
Ian Halstead <ianhalstead@Ians-MacBook-Pro-4.local>
2023-06-26 16:46:29 -0500

    frontend proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get("/test").then((d) => {
+      console.log(d);
+    });
+    console.log("Use effect");
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
