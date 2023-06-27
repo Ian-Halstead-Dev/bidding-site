975864184c53ee6d55faabc91c52bfc6155282ae
Ian Halstead <ianhalstead@Ians-MacBook-Pro-4.local>
2023-06-27 00:20:22 -0500

    cleaned some files and added better comments

--- backend/models/auction.model.js
+++ backend/models/auction.model.js
@@ -1,11 +1,28 @@
 const mongoose = require("mongoose");
 
 const auctionSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  currentBid: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  currentBidAmount: { type: Number, default: 0 },
-  // End date is the time the bid ends. If the user does not provide a date it ends right away.
-  endDate: { type: Number, default: Math.floor(Date.now().getTime() / 1000) },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  currentBid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  currentBidAmount: {
+    type: Number,
+    default: 0,
+  },
+
+  /* 
+  End date is the time the bid ends as unix time (
+    https://en.wikipedia.org/wiki/Unix_time#:~:text=Unix%20time%20is%20a%20date,made%20due%20to%20leap%20seconds.
+  If the user does not provide a date it ends right away.
+  */
+  endDate: {
+    type: Number,
+    default: Math.floor(Date.now().getTime() / 1000),
+  },
 });
 
 const Auction = mongoose.model("User", auctionSchema);
